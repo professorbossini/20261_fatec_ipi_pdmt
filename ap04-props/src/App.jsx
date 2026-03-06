7385b2956994ba89c89f04c96c818416188dc480
Rodrigo Bossini <professorbossini@gmail.com>
2026-03-06 16:12:45 -0300

    learn(ap04): encerra secao 2.5
    - retomar refatorando por meio da criacao do componente Pedido

--- ap04-props/src/App.jsx
+++ ap04-props/src/App.jsx
@@ -11,16 +11,72 @@ const App = () => (
     <div className="row">
       {/* .col-sm-12.col-lg-6.col-xxl-3*4    */}
       <div className="col-sm-12 col-lg-6 col-xxl-3">
-        <div className="py-2 border">Pedido 1</div>
+        {/* .card(.card-header+.card-body) */}
+        <div className="card">
+          <div className="card-header text-muted">15/04/2025</div>
+          <div className="card-body d-flex">
+            {/* i.fa-solid.fa-alarm-clock */}
+            {/* .d-flex.align-items-center */}
+            <div className="d-flex align-items-center">
+              <i className="fa-solid fa-alarm-clock fa-2x"></i>
+            </div>
+            <div className="ms-2 flex-grow-1 border rounded p-2">
+              <h4 className="text-center">Despertador</h4>
+              <p className="text-center">Despertador analógico</p>
+            </div>
+          </div>
+        </div>  
+
       </div>
       <div className="col-sm-12 col-lg-6 col-xxl-3">
-        <div className="py-2 border">Pedido 2</div>
+        <div className="card">
+          <div className="card-header text-muted">16/04/2025</div>
+          <div className="card-body d-flex">
+            {/* i.fa-solid.fa-alarm-clock */}
+            {/* .d-flex.align-items-center */}
+            <div className="d-flex align-items-center">
+              <i className="fa-solid fa-shirt fa-2x"></i>
+            </div>
+            <div className="ms-2 flex-grow-1 border rounded p-2">
+              <h4 className="text-center">Camiseta</h4>
+              <p className="text-center">Camiseta preta</p>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="col-sm-12 col-lg-6 col-xxl-3">
-        <div className="py-2 border">Pedido 3</div>
+        <div className="card">
+          <div className="card-header text-muted">15/04/2025</div>
+          <div className="card-body d-flex">
+            {/* i.fa-solid.fa-alarm-clock */}
+            {/* .d-flex.align-items-center */}
+            <div className="d-flex align-items-center">
+              <i className="fa-solid fa-bowling-ball fa-2x"></i>
+            </div>
+            <div className="ms-2 flex-grow-1 border rounded p-2">
+              <h4 className="text-center">Bola de boliche</h4>
+              <p className="text-center">Uma bola de boliche</p>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="col-sm-12 col-lg-6 col-xxl-3">
-        <div className="py-2 border">Pedido 4</div>
+        
+          <div className="card">
+          <div className="card-header text-muted">15/04/2025</div>
+          <div className="card-body d-flex">
+            {/* i.fa-solid.fa-alarm-clock */}
+            {/* .d-flex.align-items-center */}
+            <div className="d-flex align-items-center">
+              <i className="fa-solid fa-bicycle fa-2x"></i>
+            </div>
+            <div className="ms-2 flex-grow-1 border rounded p-2">
+              <h4 className="text-center">Bicicleta</h4>
+              <p className="text-center">Uma bicicleta 18 marchas</p>
+            </div>
+          </div>
+        
+        </div>
        </div>   
     </div>
   </div>
